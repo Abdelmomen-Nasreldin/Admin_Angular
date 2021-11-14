2f8dfabdd204c473e1304c6f6e8027d163257825
Abdelmomen <abdelmomennasreldin@gmail.com>
2021-11-14 18:20:48 +0200

    admin_v1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {path: 'Home', component : FooterComponent},
+  // {path: '', redirectTo : '/Home', pathMatch :"full"},
+  {
+    path: 'User',
+    loadChildren: () => import('./components/user/user.module').then(m => m.UserModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
